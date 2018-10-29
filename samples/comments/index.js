2d699eaddba23be242a9a93bbb3d3820a9934903
zhangsongqiang <zhangsongqiang@modeloapp.com>
2018-10-29 18:19:06 +0800

    add comment sample

--- samples/comments/index.js
+++ samples/comments/index.js
@@ -1,4 +1,4 @@
-Modelo.init({"endpoint": "https://build-portal.modeloapp.com"});
+Modelo.init({ "endpoint": "https://build-portal.modeloapp.com" });
 
 var modelId = "93rjxWY4";
 var appToken = 'c2FtcGxlcyxtb2RlbG9TQU1QTEVT'; // A sample app token
@@ -11,78 +11,191 @@ Modelo.Auth.signIn(appToken,
         var h = c.clientHeight;
         var viewer = new Modelo.View.Viewer3D(c, false, w, h);
 
-        window.addEventListener("resize",function() {
+        window.addEventListener("resize", function () {
             var c = document.getElementById("model");
             var w = c.clientWidth;
             var h = c.clientHeight;
             viewer.resize(w, h);
         });
 
-        var commentId = "";
+        // var commentId = "";
         var addComment = true;
-   
+
         viewer.loadModel(modelId, // Load the model into the viewer.
             null,
             function () {
                 var mouse = new Modelo.View.Input.Mouse(c);
                 viewer.addInput(mouse); // Add mouse to control camera.
-
-                mouse.addMouseUpListener(function(mouse) {
-                    if (addComment) {
-                        if (!mouse.moved) {
-                            Modelo.Model.createComment(modelId, 
-                                "test", "a test comment", "", 
-                                function(commentId1) { commentId = commentId1; console.log("comment created"); },
-                                function(errmsg) { console.log(errmsg); },
-                                viewer, mouse.x, mouse.y
-                            );
-
-                            addComment = false;
+                var addNewComment = document.getElementById("addNewComment");
+                addNewComment.onclick = function () {
+                    addComment = true;
+                    mouse.addMouseUpListener(function (mouse) {
+                        if (addComment) {
+                            if (!mouse.moved) {
+                                Modelo.Model.createComment(
+                                    modelId,
+                                    "test",
+                                    "new comment",
+                                    "",
+                                    function (commentId1) {
+                                        commentId = commentId1;
+                                        console.log("comment created");
+                                    },
+                                    function (errmsg) {
+                                        console.log(errmsg);
+                                    },
+                                    viewer,
+                                    mouse.x,
+                                    mouse.y
+                                );
+                                addComment = false;
+                            }
                         }
-                    }
-                });
-
+                    });
+                }
+                
                 var keyboard = new Modelo.View.Input.Keyboard(c); // Add keyboard callback.
                 viewer.addInput(keyboard);
-                
-                var commentId = 93;
-                var comment = null;
-                            
-                Modelo.Model.getComments(modelId, 
-                    function onSuccess(comments) {
-                        for (var i = 0; i < comments.length; i++) {
-                            console.log(comments[i]);
-                        }
 
-                        comment = comments[0];
+                var parentWrapper = document.getElementsByClassName("wrapper")[0];
+            
+                Modelo.Model.getComments(
+                    modelId,
+                    function onSuccess(comments) {
+                        for (let i = 0; i < comments.length; i++) {
+                            var divUiMessage = document.createElement("div");
+                            divUiMessage.id = i;
+                            divUiMessage.className = "ui message";
+                            divUiMessage.style.marginTop = "5px";
+                            divUiMessage.style.marginBottom = "5px";
+                            divUiMessage.onclick = function () {
+                                activateComment(comments[i]);
+                            }
+                            var divHeader = document.createElement("div");
+                            divHeader.className = "header";
+                            divHeader.innerHTML = "ID:" + comments[i].id;
+                            var divUiList = document.createElement("div");
+                            divUiList.className = "ui list";
+                            var itemSubject = document.createElement("div");
+                            itemSubject.className = "item";
+                            itemSubject.innerHTML = "Subject:";
+                            var inputSubjectWrapper = document.createElement("div");
+                            inputSubjectWrapper.className = "ui mini input";
+                            inputSubjectWrapper.style.fontSize = "1rem";
+                            var inputSubject = document.createElement("input");
+                            inputSubject.value = comments[i].subject;
+                            var itemMessage = document.createElement("div");
+                            itemMessage.className = "item";
+                            itemMessage.innerHTML = "Message:";
+                            var inputMessageWrapper = document.createElement("div");
+                            inputMessageWrapper.className = "ui mini input";
+                            inputMessageWrapper.style.fontSize = "1rem";
+                            var inputMessage = document.createElement("input");
+                            inputMessage.value = comments[i].message;
+                            var itemCamera = document.createElement("div");
+                            itemCamera.className = "item";
+                            itemCamera.innerHTML = "Camera: " + comments[i].renderData.camera;
+                            var itemCreateBy = document.createElement("div");
+                            itemCreateBy.className = "item";
+                            itemCreateBy.innerHTML = "CreatedBy:" + comments[i].userData;
+                            var itemCreateAt = document.createElement("div");
+                            itemCreateAt.className = "item";
+                            var stopIndex = comments[i].createdAt.indexOf("GMT");
+                            itemCreateAt.innerHTML = "CreatedAt: " + comments[i].createdAt.substring(0, stopIndex - 9);
+                            var deleteBtn = document.createElement("button");
+                            deleteBtn.className = "mini ui red button";
+                            deleteBtn.innerHTML = "Delete";
+                            deleteBtn.onclick = function () {
+                                // miss deleteErr
+                                this.parentNode.parentNode.removeChild(this.parentNode);
+                                var currentCommentId = comments[i].id;
+                                deleteComment(currentCommentId);
+                            }
+                            var updateBtn = document.createElement("button");
+                            updateBtn.className = "mini ui blue button";
+                            updateBtn.innerHTML = "Update";
+                            updateBtn.style.marginLeft = "74px";
+                            updateBtn.onclick = function () {
+                                var currentCommentId = comments[i].id;
+                                var currentSubject = this.parentNode.childNodes[1].childNodes[1].firstElementChild.value;;
+                                var currentMessage = this.parentNode.childNodes[1].childNodes[3].firstElementChild.value;
+                                updateComment(currentCommentId, currentSubject, currentMessage);
+                            }
+                            parentWrapper.appendChild(divUiMessage);
+                            divUiMessage.appendChild(divHeader);
+                            divUiMessage.appendChild(divUiList);
+                            divUiList.appendChild(itemSubject);
+                            divUiList.appendChild(inputSubjectWrapper);
+                            inputSubjectWrapper.appendChild(inputSubject);
+                            inputSubjectWrapper.style.width = "100%";
+                            divUiList.appendChild(itemMessage);
+                            divUiList.appendChild(inputMessageWrapper);
+                            inputMessageWrapper.appendChild(inputMessage);
+                            inputMessageWrapper.style.width = "100%";
+                            divUiList.appendChild(itemCamera);
+                            divUiList.appendChild(itemCreateBy);
+                            divUiList.appendChild(itemCreateAt);
+                            divUiMessage.appendChild(deleteBtn);
+                            divUiMessage.appendChild(updateBtn);
+                        }
+                        console.log(comments);
                     },
                     function onFail(errmsg) {
-                        console.log(errmsg);
+                        console.log('getCommentsErr: ' + errmsg);
                     });
+                function updateComment(commentId, subject, message) {
+                    Modelo.Model.updateComment(
+                        commentId,
+                        subject,
+                        message,
+                        function (commentId) {
+                            console.log(commentId + "comment updated");
+                        },
+                        function (errmsg) {
+                            console.log("updateErr:" + errmsg);
+                        },
+                    );
+                }
+                function deleteComment(commentId) {
+                    Modelo.Model.deleteComment(
+                        commentId,
+                        function (commentId) {
+                            console.log(commentId + "comment deleted");
+                        },
+                        function (errmsg) {
+                            console.log('DeleteErr: ' + errmsg);
+                        },
+                    );
+                }
+                function activateComment(comment) {
+                    Modelo.Model.activateComment(comment, viewer);
+                }
 
-                keyboard.addKeyUpListener(function (keyboard) {
-                    switch (keyboard.key) {
-                        case 65: //'a'
-                            addComment = true;
-                            break;
-                        case 66: //'b'
-                            Modelo.Model.deleteComment(commentId, 
-                                function(commentId) { console.log("comment deleted"); },
-                                function(errmsg) { console.log(errmsg); },
-                            );
-                            break;
-                        case 67: //'c'
-                            Modelo.Model.updateComment(commentId, 
-                                "test1", "another test comment",
-                                function(commentId) { console.log("comment updated"); },
-                                function(errmsg) { console.log(errmsg); },
-                            );
-                            break;
-                        case 69: //'e'
-                            Modelo.Model.activateComment(comment, viewer); 
-                            break;
-                    }
-                }); 
+
+
+                // keyboard.addKeyUpListener(function (keyboard) {
+                //     switch (keyboard.key) {
+                //         case 65: //'a'
+                //             addComment = true;
+                //             break;
+                //         case 66: //'b'
+                //             Modelo.Model.deleteComment(commentId,
+                //                 function (commentId) { console.log("comment deleted"); },
+                //                 function (errmsg) { console.log(errmsg); },
+                //             );
+                //             break;
+                //         case 67: //'c'
+                //             Modelo.Model.updateComment(commentId,
+                //                 "test2", "new test comment",
+                //                 function (commentId) { console.log("comment updated"); },
+                //                 function (errmsg) { console.log(errmsg); },
+                //             );
+                //             break;
+                //         case 69: //'e'
+                //             Modelo.Model.activateComment(comment, viewer);
+                //             break;
+                //     }
+                // });
                 console.log("done");
             },
             function (errmsg) {
