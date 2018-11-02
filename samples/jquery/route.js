911175163aff0eb831921e75ae35bee3ff84b71b
zhangsongqiang <zhangsongqiang@modeloapp.com>
2018-11-02 15:23:36 +0800

    add jquery-sample

---
+++ samples/jquery/route.js
@@ -0,0 +1,17 @@
+$(function () {
+  var router = {
+    index: function () {
+      $('.container').load('./modeloPage.html')
+    },
+    yourPage: function () {
+      $('.container').load('./yourPage.html')
+    }
+  }
+
+  var routes = {
+    '/index': router.index,
+    '/yourPage': router.yourPage,
+  };
+  var router = Router(routes);
+  router.init('/index');//初始化页面
+})
