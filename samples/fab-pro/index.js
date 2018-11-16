51e35e86045512cc2841390eb7eedcd1181fe6fe
nxddsnc <fanqileiOGL@163.com>
2018-11-16 09:09:15 +0800

    stash

---
+++ samples/fab-pro/index.js
@@ -0,0 +1,44 @@
+Modelo.init({ "endpoint": "https://build-portal.modeloapp.com" });
+
+var modelId = "93rjxWY4";
+var appToken = 'c2FtcGxlcyxtb2RlbG9TQU1QTEVT'; // A sample app token
+
+Modelo.Auth.signIn(appToken,
+    function () {
+        var viewer = new Modelo.View.Viewer3D("model");
+
+        viewer.addInput(new Modelo.View.Input.Mouse(viewer)); // Add mouse to control camera.
+        var selectElementTool = new Modelo.View.Tool.SelectElements(viewer);
+        viewer.addTool(selectElementTool);
+        selectElementTool.setEnabled(true);
+        selectElementTool.setMultiselectEnabled(true);
+        viewer.getEventEmitter().on("onElementSelected", (elementNames1) => {
+
+        });
+        
+        viewer.loadModel(modelId, // Load the model into the viewer.
+            null,
+            function () {
+
+
+                var keyboard = new Modelo.View.Input.Keyboard(viewer); // Add keyboard callback.
+                viewer.addInput(keyboard);
+                keyboard.addKeyUpListener(function (keyboard) {
+                    if (keyboard.key === 27) {
+                        viewer.destroy();
+                    }
+                });
+                console.log("done");
+            },
+            function (errmsg) {
+                console.log(errmsg); // The loading error.
+            },
+            function (per) {
+                var c = document.getElementById("progress");
+                c.innerHTML = "Loading: " + Math.round(per * 100) + "%";
+            });
+    },
+    function (errmsg) {
+        console.log(errmsg); // If there is any sign-inerror.
+    });
+
