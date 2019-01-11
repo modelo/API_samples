a3e743a72e43b9804072f8c33d10f5dc7f99c6ec
joshua7v <joshua7v@hotmail.com>
2019-01-11 11:35:35 +0800

    update basic sample to promise-based api

--- samples/hello-world/index.js
+++ samples/hello-world/index.js
@@ -1,34 +1,33 @@
-Modelo.init({ "endpoint": "https://build-portal.modeloapp.com" });
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com" });
 
-var modelId = "93rjxWY4";
-var appToken = 'c2FtcGxlcyx0ZVNhbXBsZVBhc3M1NDE='; // A sample app token
+const modelId = "93rjxWY4";
+// A sample app token
+const appToken = "c2FtcGxlcyx0ZVNhbXBsZVBhc3M1NDE=";
 
-Modelo.Auth.signIn(appToken,
-    function () {
-        var viewer = new Modelo.View.Viewer3D("model");
+Modelo.Auth.signIn(appToken)
+    .then(() => {
+        const viewer = new Modelo.View.Viewer3D("model");
 
-        viewer.loadModel(modelId, // Load the model into the viewer.
-            null,
-            function () {
-                viewer.addInput(new Modelo.View.Input.Mouse(viewer)); // Add mouse to control camera.
-                var keyboard = new Modelo.View.Input.Keyboard(viewer); // Add keyboard callback.
+        viewer
+            .loadModel(modelId, progress => {
+                // second parameter is an optional progress callback
+                const c = document.getElementById("progress");
+                c.innerHTML = "Loading: " + Math.round(progress * 100) + "%";
+            })
+            .then(() => {
+                // model loaded successfully
+
+                // add mouse to control camera.
+                viewer.addInput(new Modelo.View.Input.Mouse(viewer));
+                // add keyboard callback.
+                const keyboard = new Modelo.View.Input.Keyboard(viewer);
+
                 viewer.addInput(keyboard);
-                keyboard.addKeyUpListener(function (keyboard) {
+                keyboard.addKeyUpListener(keyboard => {
                     if (keyboard.key === 27) {
                         viewer.destroy();
                     }
                 });
-                console.log("done");
-            },
-            function (errmsg) {
-                console.log(errmsg); // The loading error.
-            },
-            function (per) {
-                var c = document.getElementById("progress");
-                c.innerHTML = "Loading: " + Math.round(per * 100) + "%";
             });
-    },
-    function (errmsg) {
-        console.log(errmsg); // If there is any sign-inerror.
-    });
-
+    })
+    .catch(e => console.log(e.message));
