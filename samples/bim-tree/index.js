d99cb374ca912320712b5f1a19dfbff63840de6c
Xiaoguai <31888190+AkiyamaMinami@users.noreply.github.com>
2019-03-25 13:37:55 +0800

    MOD-13676 add compare models and sketch samples (#9)

--- samples/bim-tree/index.js
+++ samples/bim-tree/index.js
@@ -1,23 +1,24 @@
-const searchValue = document.getElementById("searchValue");
-const searchBtn = document.getElementById("searchBtn");
-const show = document.getElementById("bimTree-show");
+var searchValue = document.getElementById("searchValue");
+var searchBtn = document.getElementById("searchBtn");
+var show = document.getElementById("bimTree-show");
 
-const appToken = ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE'; // A sample app token  
-Modelo.init({ "endpoint": "https://build-portal.modeloapp.com", appToken });
+var appToken =
+  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE"; // A sample app token
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
 
 searchBtn.onclick = () => {
-    searchBtn.className = "ui loading button";
-    show.innerHTML = "";
-    const searchId = searchValue.value || searchValue.placeholder;
-    Modelo.BIM.getTreeInfo(searchId).then((bimTree) => {
-        const bimTreeData = JSON.stringify(bimTree, null, 2);
-        show.innerHTML = bimTreeData;
-        searchBtn.className = "ui button";
-    }).catch(e => {
-        show.innerHTML = "No BIM Tree";
-        searchBtn.className = "ui button";
-        console.log('getBIMTreeErr: ' + e);
+  searchBtn.className = "ui loading button";
+  show.innerHTML = "";
+  var searchId = searchValue.value || searchValue.placeholder;
+  Modelo.BIM.getTreeInfo(searchId)
+    .then(bimTree => {
+      var bimTreeData = JSON.stringify(bimTree, null, 2);
+      show.innerHTML = bimTreeData;
+      searchBtn.className = "ui button";
+    })
+    .catch(e => {
+      show.innerHTML = "No BIM Tree";
+      searchBtn.className = "ui button";
+      console.log("getBIMTreeErr: " + e);
     });
-}
-
-
+};
--- samples/upload-model/index.js
+++ samples/upload-model/index.js
@@ -6,34 +6,37 @@ var progressShow = document.getElementById("progressShow");
 var $progress = $("#progress");
 var modelFile = null;
 
-var appToken = ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE'; // A sample app token
-Modelo.init({ "endpoint": "https://build-portal.modeloapp.com", appToken });
+var appToken =
+  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE"; // A sample app token
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
 uploadBtn.onclick = () => {
-    fileInput.click();
-    fileInput.onchange = () => {
-        modelFile = fileInput.files[0];
-        $progress.progress('reset');
-        // Upload a model. Params: folderId file
-        Modelo.Model.upload(modelFile, (percentage) => {
-            var percentage = Math.ceil(percentage * 100);
-            var currentProgressValue = $progress.progress("get value");
-            fileInput.disabled = "disabled";
-            uploadBtn.className = "ui primary loading button";
-            for (var i = currentProgressValue; i < percentage; i++) {
-                $progress.progress("increment");
-            };
-            console.log('progress: ' + percentage);
-        }).then((modelId) => {
-            showFileName.innerHTML = modelFile.name;
-            fileInput.disabled = "";
-            uploadBtn.className = "ui primary button";
-            console.log(modelId);
-        }).catch((e) => {
-            fileInput.disabled = "";
-            uploadBtn.className = "ui primary button";
-            progress.className = "ui small progress error";
-            progressShow.innerHTML = "Uploading Error";
-            console.log('uploadErr: ' + e);
-        });
-    }
-}
+  fileInput.click();
+  fileInput.onchange = () => {
+    modelFile = fileInput.files[0];
+    $progress.progress("reset");
+    // Upload a model. Params: folderId file
+    Modelo.Model.upload(modelFile, percentage => {
+      var percentage = Math.ceil(percentage * 100);
+      var currentProgressValue = $progress.progress("get value");
+      fileInput.disabled = "disabled";
+      uploadBtn.className = "ui primary loading button";
+      for (var i = currentProgressValue; i < percentage; i++) {
+        $progress.progress("increment");
+      }
+      console.log("progress: " + percentage);
+    })
+      .then(modelId => {
+        showFileName.innerHTML = modelFile.name;
+        fileInput.disabled = "";
+        uploadBtn.className = "ui primary button";
+        console.log(modelId);
+      })
+      .catch(e => {
+        fileInput.disabled = "";
+        uploadBtn.className = "ui primary button";
+        progress.className = "ui small progress error";
+        progressShow.innerHTML = "Uploading Error";
+        console.log("uploadErr: " + e);
+      });
+  };
+};
