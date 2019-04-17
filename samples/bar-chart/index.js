aebff9f3caece3e1bf6315c8273c69bb88bea0f5
nxddsnc <fanqileiOGL@163.com>
2019-04-17 17:54:13 +0800

    Make height map and bar chart work again

--- samples/bar-chart/index.js
+++ samples/bar-chart/index.js
@@ -18,7 +18,7 @@ keyboard.addKeyUpListener(keyboard => {
     }
 });
     
-const barchart = new Modelo.ThreeDScene.Visualize.BarChart(viewer.getRenderScene());
+const barchart = new Modelo.Scene3D.Visualize.BarChart(viewer.getRenderScene());
 viewer.getScene().addVisualize(barchart);
 
 barchart.setParameter("xres", 50);
@@ -26,68 +26,19 @@ barchart.setParameter("yres", 50);
 barchart.setPosition([-50, -100, 10]);
 barchart.setScale([50, 50, 50]);
 
-var rawData = [
-    {
-      "x": 0.09803921568627451,
-      "y": 0.5175757575757576
-    },
-    {
-      "x": 0.22941176470588234,
-      "y": 0.7109090909090909
-    },
-    {
-      "x": 0.17294117647058824,
-      "y": 0.6733333333333333
-    },
-    {
-      "x": 0.21568627450980393,
-      "y": 0.5054545454545455
-    },
-    {
-      "x": 0.12117647058823529,
-      "y": 0.6545454545454545
-    },
-    {
-      "x": 0.04156862745098039,
-      "y": 0.5212121212121212
-    },
-    {
-      "x": 0.08392156862745098,
-      "y": 0.7854545454545454
-    },
-    {
-      "x": 0.10941176470588235,
-      "y": 0.7745454545454545
-    },
-    {
-      "x": 0.09450980392156863,
-      "y": 0.6157575757575757
-    },
-    {
-      "x": 0.10549019607843137,
-      "y": 0.7303030303030303
-    },
-    {
-      "x": 0.07333333333333333,
-      "y": 0.6642424242424242
-    },
-    {
-      "x": 0.0792156862745098,
-      "y": 0.7557575757575757
-    },
-    {
-      "x": 0.08784313725490196,
-      "y": 0.7563636363636363
-    }
-];
-
 //
 // Heatmap map
 //
-const heatmap = new Modelo.ThreeDScene.Visualize.HeatMap(viewer.getRenderScene());
+const heatmap = new Modelo.Scene3D.Visualize.HeatMap(viewer.getRenderScene());
 viewer.getScene().addVisualize(heatmap);
 
-heatmap.setParameter("points", rawData);
+var data = rawData.data;
+for (var i = 0 ; i < data.length; i++) {
+  data[i].x = parseFloat(data[i].X) / rawData.width;
+  data[i].y = parseFloat(data[i].Y) / rawData.height;
+}
+
+heatmap.setParameter("points", data);
 heatmap.setParameter("width", 256);
 heatmap.setParameter("height", 256);
 heatmap.setParameter("gridSize", 64);
