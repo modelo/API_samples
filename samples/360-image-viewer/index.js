8069e39ab7a758df5dc0f6cc138aacc6c0af79bc
joshua7v <joshua7v@hotmail.com>
2019-06-24 15:11:27 +0800

    NO-MOD update 360 image

--- samples/360-image-viewer/index.js
+++ samples/360-image-viewer/index.js
@@ -1,4 +1,4 @@
-var assetId = 475;
+var assetId = 554;
 var appToken =
   " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE";
 
