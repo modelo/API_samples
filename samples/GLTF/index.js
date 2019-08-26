df39c4d224a9cadd6664e69f732be34b7107d6d4
nxddsnc <fanqileiOGL@163.com>
2019-08-26 14:17:59 +0800

    gltf

---
+++ samples/GLTF/index.js
@@ -0,0 +1,28 @@
+var modelId = "G8z6zQ8j";
+var appToken =
+  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsInVzZXJuYW1lIjoic2FtcGxlcyIsImlhdCI6MTU0ODE0NjI3NywiZXhwIjozMzA4NDE0NjI3N30.XoUmS8836nUVm0mASqL6qiaXgg34Xn4lyieaPtrn5mE";
+
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
+
+var viewer = new Modelo.View.Viewer3D("model");
+
+viewer.loadGLTFModel()
+viewer
+  .loadModel(modelId, progress => {
+    // second parameter is an optional progress callback
+    var c = document.getElementById("progress");
+    c.innerHTML = "Loading: " + Math.round(progress * 100) + "%";
+  })
+  .then(() => {
+    // model loaded successfully
+    // add mouse to control camera.
+    viewer.addInput(new Modelo.View.Input.Mouse(viewer));
+    // add keyboard callback.
+    var keyboard = new Modelo.View.Input.Keyboard(viewer);
+    viewer.addInput(keyboard);
+    keyboard.addKeyUpListener(keyboard => {
+      if (keyboard.key === 27) {
+        viewer.destroy();
+      }
+    });
+  });
