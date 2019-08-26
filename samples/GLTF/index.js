f15ff468ebc4b1fd5f3133c3f9bc6e5c3dd5554e
nxddsnc <fanqileiOGL@163.com>
2019-08-26 17:08:27 +0800

    gltf

--- samples/GLTF/index.js
+++ samples/GLTF/index.js
@@ -6,23 +6,5 @@ Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
 
 var viewer = new Modelo.View.Viewer3D("model");
 
-viewer.loadGLTFModel()
-viewer
-  .loadModel(modelId, progress => {
-    // second parameter is an optional progress callback
-    var c = document.getElementById("progress");
-    c.innerHTML = "Loading: " + Math.round(progress * 100) + "%";
-  })
-  .then(() => {
-    // model loaded successfully
-    // add mouse to control camera.
-    viewer.addInput(new Modelo.View.Input.Mouse(viewer));
-    // add keyboard callback.
-    var keyboard = new Modelo.View.Input.Keyboard(viewer);
-    viewer.addInput(keyboard);
-    keyboard.addKeyUpListener(keyboard => {
-      if (keyboard.key === 27) {
-        viewer.destroy();
-      }
-    });
-  });
+viewer.loadGLTFModel("http://localhost:8080/samples/GLTF/Avocado/Avocado.gltf");
+viewer.addInput(new Modelo.View.Input.Mouse(viewer));
