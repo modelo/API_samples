06efc474ce5d58bc829a5ce7b48d64d0e215d4c4
nxddsnc <fanqileiOGL@163.com>
2019-09-03 13:13:53 +0800

    add multi-layer-volume sample

---
+++ samples/multi-layer-volume/index.js
@@ -0,0 +1,70 @@
+const modelId = "x1qwRd8W";
+const appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjUsInVzZXJuYW1lIjoiZnFsIiwiaWF0IjoxNTQ4Mjk4NDIxLCJleHAiOjMzMDg0Mjk4NDIxfQ.-ZNOLrw1W9OOf9iG8QkgZuFJR5JUJmHDZvkZLsdR15Y";
+
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
+
+const viewer = new Modelo.View.Viewer3D("model");
+
+viewer.addInput(new Modelo.View.Input.Mouse(viewer));
+const keyboard = new Modelo.View.Input.Keyboard(viewer);
+viewer.addInput(keyboard);
+keyboard.addKeyUpListener(keyboard => {
+    if (keyboard.key === 27) {
+        viewer.destroy();
+    }
+});
+    
+const heatmap = new Modelo.View.Visualize.HeatMap(viewer.getRenderScene());
+viewer.getScene().addVisualize(heatmap);
+heatmap.setParameter("width", 256);
+heatmap.setParameter("height", 256);
+heatmap.setParameter("gridSize", 32);
+
+var data = [];
+var imageDatas = [];
+for (var i = 0; i < 64; i++) {
+    
+    for (var j = 0; j < 20; j++) {
+        data[j] = {
+            x: Math.random(),
+            y: Math.random(),
+            number: Math.random() * 10
+        }
+    }
+    heatmap.setParameter("points", data);
+    var imageData = heatmap.getImageData();
+    imageDatas.push(new Float32Array(imageData));
+}
+
+var volumeRenderingData = new Float32Array(2048*2048);
+for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+        // var offset = i * 256 * 256 * 8 + j * 256 * 256;
+        for (var ii = 0; ii < 256; ii++) {
+            for (var jj = 0; jj < 256; jj++) {
+                volumeRenderingData[i * 256 * 256 * 8 + ii * 256 * 8 + j * 256 + jj] = imageDatas[i * 8 + j][ii * 256 + jj];
+
+                if (ii > 56 && ii < 200 && jj > 56 && jj < 200) {
+                    volumeRenderingData[i * 256 * 256 * 8 + ii * 256 * 8 + j * 256 + jj] = 0;
+                }
+            }
+        }
+    }
+}
+
+const volume = new Modelo.View.Visualize.MultiLayerVolume(viewer.getRenderScene());
+viewer.getScene().addVisualize(volume);
+
+volume.setParameter("platteImage", "platte.png");
+volume.setParameter("data", { "data": volumeRenderingData, "width": 2048, "height": 2048 });
+volume.setParameter("layer3", 64);
+volume.setScaling([10, 10, 20]);
+volume.setPosition([0, 0, 7.7]);
+volume.setRotation([0, 0, 1], 0.5);
+// Create ground geometry
+var ground = new Modelo.View.Pawn("ground", viewer.getResourceManager(), viewer.getMaterialManager());
+ground.createSolidCube();
+ground.setScaling(25, 25, 0.25);
+viewer.getScene().addPawn(ground);
+        
+volume.setEnabled(true);
