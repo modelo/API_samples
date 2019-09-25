d573ec156f823a6e0a733b305d7283c61573eed9
nxddsnc <fanqileiOGL@163.com>
2019-09-25 16:51:41 +0800

    Update samples:

--- samples/people-location-analysis/index.js
+++ samples/people-location-analysis/index.js
@@ -9,17 +9,21 @@ Modelo.init({
 
 const viewer = new Modelo.View.Viewer3D("model");
 viewer.setRenderingLinesEnabled(true);
+
+
+
 viewer.loadModel(modelId, progress => {
   // /assets/js/utils.js
   updateProgress(progress);
 }).then(() => {
     
-    const bbox = [-89.5673828125, -83.00537109375, -161.33059692382812, 89.56739807128906, -9.317974090576172, -92.43255615234375];
+    const bbox =  [-89.5673828125, -83.00537109375, -161.33059692382812, 89.56739807128906, -49.967777252197266, -92.43257904052734];
     setCommonDark(viewer);
     viewer.addTool(new Modelo.View.Tool.Section(viewer));
     viewer.addInput(new Modelo.View.Input.Mouse(viewer));
     viewer.addInput(new Modelo.View.Input.Touch(viewer));
     const keyboard = new Modelo.View.Input.Keyboard(viewer);
+
     viewer.addInput(keyboard);
     keyboard.addKeyUpListener(keyboard => {
         if (keyboard.key === 27) {
@@ -30,47 +34,55 @@ viewer.loadModel(modelId, progress => {
     const sectionTool = viewer.getTool('Section');
     sectionTool.setEnabled(true);
     sectionTool.setSectionBox(bbox);
-    
+    sectionTool.setInteractive(true);
     const metaballConfig = {
         width: 512,
-        height: 512,
+        height: 128,
         contour: true,
         contourColor:  [1, 0, 0, 1.0],
-        backgroundColor: [1, 1, 1, 0.0],
+        backgroundColor: [1, 1, 1, 1],
         color: [1.0, 0.45, 0.45, 1.0]
     };
     modeloMetaball(rawData, metaballConfig);
 });
 
 
-function modeloMetaball(points, config) {
+function modeloMetaball(data, config) {
     // viewer.getTool('Section').setEnabled(false);
-    points.forEach(point => {
-        point.x = point.x / 100;
-        point.y = point.y / 100;
-    })
+    var points = data.points;
+    var min = data.min, max = data.max;
+    var relativePoints = [];
+    points.forEach((point, index) => {
+        relativePoints[index] = {
+            x: (point.x - min[0]) / (max[0] - min[0]),
+            y: (point.y - min[1]) / (max[1] - min[1])
+        }
+    });
+
     const metaball = new Modelo.View.Visualize.MetaBall(viewer.getRenderScene());
-    metaball.setParameter("points", points);
+    metaball.setParameter("points", relativePoints);
     metaball.setParameter("width", config.width);
     metaball.setParameter("height", config.height);
     metaball.setParameter("contour", config.contour);
     metaball.setParameter("contourColor", config.contourColor);
     metaball.setParameter("backgroundColor", config.backgroundColor);
     metaball.setParameter("color", config.color);
-
-    const groundPlane = new Modelo.View.Pawn("ground2", viewer.getResourceManager(), viewer.getMaterialManager());
-    groundPlane.createTexturedQuad([metaball.getTexture()]);
-    groundPlane.setScaling(40, 40, 1.0);
-    groundPlane.setTranslation(0, 0, 1.01);
-    viewer.getScene().addPawn(groundPlane);
+    metaball.setParameter("radius", 16);
 
     for (let i = 0; i < points.length; i++) {
         const cube = new Modelo.View.Pawn("cube" + i, viewer.getResourceManager(), viewer.getMaterialManager());
-        cube.createSolidCube([0.8, 0.8, 0.8]);
-        const x = points[i].x - 40.0;
-        const y = points[i].y - 40.0;
-        cube.setTranslation(x, y, 2.013);
+        cube.createSolidCube();
+        const x = points[i].x / 304;
+        const y = points[i].y / 304;
+        cube.setTranslation(x, y, 1.8);
         cube.setScaling(0.6, 0.6, 0.6);
         viewer.getScene().addPawn(cube);
     }
+    const groundPlane = new Modelo.View.Pawn("ground2", viewer.getResourceManager(), viewer.getMaterialManager());
+    groundPlane.createTexturedQuad([metaball.getTexture()]);
+
+    max[0] /= 304; max[1] /= 304; min[0] /= 304; min[1] /= 304;
+    groundPlane.setScaling((max[0] - min[0]) * 0.5, (max[1] - min[1]) * 0.5, 1.0);
+    groundPlane.setTranslation((max[0] + min[0]) * 0.5, (max[1] + min[1]) * 0.5, 1.0);
+    viewer.getScene().addPawn(groundPlane);
 }
