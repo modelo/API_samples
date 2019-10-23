0f42d324d10c64f9467e1dc422c7dd37e85c7347
cczutang <tangyajun_3@live.com>
2019-10-23 15:04:20 +0800

    Update escape-route (#23)
    * Remove template files in master branch
    
    * Update modeloapi url
    
    * Update modelId of beijing
    
    * add 360 walk through sample
    
    * Add revit case
    
    * Update setting panel
    
    * Update people-location-analysis
    
    * Update sample
    
    * Update samples:
    
    * Update url
    
    * refactor
    
    * Add  urban-ensity-viz heatmap
    
    * Update urban-ensity-viz
    
    * Add hidden samples
    
    * Update urban-POI
    
    * Update metalball
    
    * Update revit-case
    
    * Update IOT-2
    
    * Add ribbon
    panel to IOT-2;
    
    * Update IOT-2
    
    * Update IOT-2
    
    * merge'
    
    * Update escape-route
    
    * Update escape-route

--- samples/escape-route/index.js
+++ samples/escape-route/index.js
@@ -28,8 +28,47 @@ viewer.loadModel(modelId, progress => {
     });
     viewer.setSmartCullingEnabled(false);
   });
-
   viewer.setLazyRenderingEnabled(false);
+
+  let pointsArray = [];
+  for (const key in escapePathData) {
+    pointsArray.push(escapePathData[key]);
+  }
+  
+  pointsArray.forEach((points, index) => {
+    points.forEach(function(point) {
+      point[0] = point[0] / 304;
+      point[1] = point[1] / 304;
+      point[2] = 1.8
+    });
+    const person = new Modelo.View.Pawn("person" + index, viewer.getResourceManager(), viewer.getMaterialManager());
+    const pathFollowingAnimator = new Modelo.View.PathFollowingAnimator(viewer, points);
+    // Load local gltf file with animation info. Note: Modelo3d only support gltf 2.0 version for now.
+    person.loadGltfModel("./scene.gltf").then(function() {
+
+      // Set the scaling and rotation of the person so that it can move forwad.
+      person.setScaling(4, 4, 4);
+      person.rotate(3.1416, [0, 0, 1], [0, 0, 0]);
+      person.setTranslation(...points[0]);
+
+      viewer.getScene().addPawn(person, false);
+      // Get the available animations.
+      const skeleton = person.getSkeletons()[0];
+      const animation = skeleton.getAnimations()[0];
+      // Set skeleton animation speed.
+      animation.setSpeed(0.02);
+      // Start animation.
+      skeleton.doAnimation(animation);
+
+      // Attach the person to the path following animation.
+      pathFollowingAnimator.addPawn(person);
+      // Set the path following animation speed.
+      pathFollowingAnimator.setSpeed(0.2);
+
+      pathFollowingAnimator.start();
+    });
+  })
+
   const ribbon = new Modelo.View.Visualize.AnimatingRibbon(viewer.getRenderScene());
   ribbon.setEnabled(true);
   viewer.getScene().addVisualize(ribbon);
@@ -37,16 +76,8 @@ viewer.loadModel(modelId, progress => {
   ribbon.setParameter("unitLenght", 1000);
   ribbon.setParameter("speed", 1);
   ribbon.setParameter("platteTexture", "platte.png");
-  let pointsArray = [];
-  for (const key in escapePathData) {
-    pointsArray.push(escapePathData[key]);
-  }
-  pointsArray.forEach(function(points) {
-    points.forEach(function(point) {
-      point[0] = point[0] / 304;
-      point[1] = point[1] / 304;
-      point[2] = 1.8
-    });
+
+  pointsArray.forEach((points) => {
     ribbon.addRibbon(points);
   });
   ribbon.setScaling(1.6, 0.6, 0.6);
