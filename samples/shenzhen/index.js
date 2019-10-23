2bf9c323c43286841215dbbf10b2390fd0933ceb
nxddsnc <fanqileiOGL@163.com>
2019-10-23 17:02:37 +0800

    Update sketch effect on mobile devices

--- samples/shenzhen/index.js
+++ samples/shenzhen/index.js
@@ -28,11 +28,15 @@ viewer
 
   viewer.setBackgroundColor([26.0 / 255.0, 26.0 / 255.0, 55.0 / 255.0, 1.0]);
 
-  viewer.setEffectEnabled("Sketch", true);
-  viewer.setEffectParameter("Sketch", "color", [180 / 255.0, 180 / 255.0, 180 / 255.0]);
-  viewer.setEffectParameter("Sketch", "colored", true);
-  //this.setEffectParameter("Sketch", "surfaceColor", [51.0 / 255.0, 145.0 / 255.0, 1.0, 0.2]);
-  viewer.setEffectParameter("Sketch", "transparents", true);
+  if (!isMobile()) {
+    viewer.setEffectEnabled("Sketch", true);
+    viewer.setEffectParameter("Sketch", "color", [180 / 255.0, 180 / 255.0, 180 / 255.0]);
+    viewer.setEffectParameter("Sketch", "colored", true);
+    viewer.setEffectParameter("Sketch", "transparents", true);
+    viewer.setEffectParameter("Sketch", "contrast", 60),
+    viewer.setEffectParameter("Sketch", "detail", 20)
+  }
+
 
   // Turn on SSAO effect
   viewer.setEffectEnabled("SSAO", true);
