d92334340c3f122b2b82fcb243bed3c99134647d
suqidan <suqidan@gmail.com>
2019-11-27 17:47:03 +0800

    update 2d-graph-interaction

--- samples/2d-graph-interaction/index.js
+++ samples/2d-graph-interaction/index.js
@@ -1,5 +1,5 @@
 let lastElement = '';
-const modelId = "q8Z27w8a";
+const modelId = "VYnWnX8J";
 const appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"
 
 Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
@@ -12,6 +12,19 @@ function updateProgress(progress) {
 const viewer = new Modelo.View.Viewer3D("model", {   isMobile: isMobile() });
 viewer.addInput(new Modelo.View.Input.Mouse(viewer)); // Add mouse to control camera.
 viewer.addInput(new Modelo.View.Input.Touch(viewer));
+
+var section = new Modelo.View.Tool.Section(viewer);
+viewer.addTool(section);
+
+var selectElementTool = new Modelo.View.Tool.SelectElements(viewer);
+viewer.addTool(selectElementTool);
+selectElementTool.setEnabled(true);
+
+viewer.loadModel(modelId, updateProgress).then(() => {
+    setDarkTheme(viewer);
+    chartSetting();
+})
+
 const colors = [
     {
         color: 'default',
@@ -30,10 +43,7 @@ const colors = [
         value: [0, 0, 1]
     }
 ];
-viewer.loadModel(modelId, updateProgress).then(() => {
-    setDarkTheme(viewer);
-    chartSetting();
-})
+
 
 
 function chartSetting() {
@@ -60,10 +70,10 @@ function chartSetting() {
                 center: ['50%', '50%'],
                 selectedMode: 'single',
                 data:[
-                    {value:elements.type1.length, name: 'type1'},
-                    {value:elements.type2.length, name: 'type2'},
-                    {value:elements.type3.length, name: 'type3'},
-                    {value:elements.type4.length, name: 'type4'},
+                    {value:elements.椅子.length, name: '椅子'},
+                    {value:elements.人.length, name: '人'},
+                    {value:elements.风车.length, name: '风车'},
+                    {value:elements.消除.length, name: '消除'},
                 ],
                 itemStyle: {
                     emphasis: {
@@ -81,10 +91,54 @@ function chartSetting() {
             viewer.getScene().setElementsColor(elements[lastElement], null);
         }
         lastElement = params.name;
-        viewer.getScene().setElementsColor(elements[lastElement], hexToRGB(params.color));
+        
+        //在相关饼图被点击时启用某个已被定义好的comments
+        if(params.name == "椅子"){
+            CameraPickandHighlight(0,elements[lastElement],params);
+        }
+        else if(params.name == "人"){
+            CameraPickandHighlight(1,elements[lastElement],params);
+        }
+        else if(params.name == "风车"){
+            CameraPickandHighlight(2,elements[lastElement],params);
+        }
+        else{
+            //关闭浮窗
+            document.getElementById("panelLabel").style.visibility = 'hidden';
+            selectElementTool.pick([], false);
+        }
+
     });
 }
 
+
+function CameraPickandHighlight(index,elementID,Parameter){
+
+    //转动照相机
+    Modelo.Comment.get(modelId).then(comments => {
+        Modelo.Comment.activate(comments[index].id);
+    })
+    
+    //变换构件颜色
+    viewer.getScene().setElementsColor(elementID, hexToRGB(Parameter.color));
+
+    //高亮构件
+    selectElementTool.pick(elementID, false);
+
+    //打开浮窗
+    document.getElementById("panelLabel").style.visibility = 'visible';
+
+    //找到构件BBOX的三维坐标，返回的是两个点，一共六个数
+    var position3D = viewer.getScene().getElementsBBox(elementID);
+
+    //用构件BBOX的坐标附上浮窗
+    viewer.setUpdateCallback(function() {
+        //从三维坐标转换到二维坐标，其中slice(3,6)选的是BBOX的后一个
+        var position2D = viewer.getCamera().project(position3D.slice(3, 6));
+        document.getElementById("panelLabel").style.left = position2D[0] + "px";
+        document.getElementById("panelLabel").style.top = position2D[1] + "px";
+    });
+}
 /**
  * hex to rgb color
  * @param {*} color 
