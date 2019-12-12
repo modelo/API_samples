02b4cd73d460addfc2076d06f80e48494974a325
suqidan <suqidan@gmail.com>
2019-12-12 15:12:41 +0800

    animating ribbon test

--- samples/animating-ribbon/index.js
+++ samples/animating-ribbon/index.js
@@ -1,9 +1,10 @@
-var modelId = "yYGeoDre";
-var appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"
+var modelId = "z8AP9mYX";
+var appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTEsInVzZXJuYW1lIjoid2FuZ2p1bnJlbiIsImlhdCI6MTU0ODI5NDMxMSwiZXhwIjozMzA4NDI5NDMxMX0.Ruz4m7XJAyYQRNDsFeJEd8Z44UqotOA-CPau4q91G2Y"
 
 Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
 
 var viewer = new Modelo.View.Viewer3DDark("model", { isMobile: isMobile() });
+var selectElementTool = new Modelo.View.Tool.SelectElements(viewer);
 
 viewer.setRenderingLinesEnabled(true);
 viewer
@@ -26,6 +27,16 @@ viewer
       }
     });
 
+    viewer.addTool(selectElementTool);
+    selectElementTool.setEnabled(true);
+    selectElementTool.setCloseUpEnabled(false);
+    selectElementTool.setHighlightEnabled(false);
+
+    //通过使用鼠标点击事件，来获得鼠标在三维中的位置,然后把返回的点放入关键帧点列中
+    viewer.getEventEmitter().on("onPointPicked", point => {
+      console.log(point);
+    })
+
     viewer.setSmartCullingEnabled(false);
   });
 
@@ -33,42 +44,14 @@ viewer.setLazyRenderingEnabled(false);
 var ribbon = new Modelo.View.Visualize.AnimatingRibbon(viewer.getRenderScene());
 ribbon.setEnabled(true);
 viewer.getScene().addVisualize(ribbon);
-ribbon.setParameter("width", 2);
-ribbon.setParameter("unitLenght", 1000);
-ribbon.setParameter("speed", 1);
-ribbon.setParameter("platteTexture", "./platte.png");
+ribbon.setParameter("width", 300);
+ribbon.setParameter("unitLenght", 20);
+ribbon.setParameter("speed", 0.5);
+ribbon.setParameter("platteTexture", "./arrowLine.png");
 
 var pointsArray = [
-  [[3432, 1485,0.3],
-  [2832, 1496,0.3],
-  [2205, 1495,0.3],
-  [1941, 1493,0.3],
-  [1910, 1482,0.3],
-  [1912, 519, 0.3]],
-  
-  [[3386, 1580, 0.3],
-  [2889,  1580, 0.3],
-  [1953,  1580, 0.3],
-  [1408,  1580, 0.3],
-  [734,   1580, 0.3],
-  [421,   1580, 0.3]],
-
-  [[421,  1520, 0.3],
-  [734,   1520, 0.3],
-  [1408,  1520, 0.3],
-  [1953,  1520, 0.3],
-  [2889,  1520, 0.3],
-  [3386,  1520, 0.3]],
-
-  [[1913, 1483, 0.3],
-  [415, 1518, 0.3]],
-  
-  [[3412, 1616, 0.3],
-  [2826, 1610, 0.3],
-  [2219, 1617, 0.3],
-  [1968, 1615, 0.3],
-  [1916, 1641, 0.3],
-  [1944, 2242, 0.3]]
+   [[223.88925170898438, 177.96585083007812, -5.224310398101807],
+  [223.94088745117188, 213.5816192626953, -5.258899688720703]]
 ]
 
 var ribbons = [];
