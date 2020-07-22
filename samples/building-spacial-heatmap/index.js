0fd90c5ec0b9702f6daa24d59900d237108dcd29
LynnYu17 <yujueaan@126.com>
2020-07-22 20:44:24 +0800

    chore: '错误操作'

--- samples/building-spacial-heatmap/index.js
+++ samples/building-spacial-heatmap/index.js
@@ -1,4 +1,4 @@
-const modelId = "p1wbbNrj";
+const modelId = "RYQvvJ8x";
 const appToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"
 
