f581ed33d44c7b575346d336cdb25069dd05430b
LynnYu17 <yujueaan@126.com>
2020-07-22 19:58:53 +0800

    fix master mistakenly deleted model

--- samples/city/index.js
+++ samples/city/index.js
@@ -1,4 +1,4 @@
-var modelId = "zY7P6G87";
+var modelId = "6YdejQ8W";
 var appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA";
 
 Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
