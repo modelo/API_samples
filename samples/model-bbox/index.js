3e4d0bc5d8665974f1a10cf1911b770d10450704
LynnYu17 <yujueaan@126.com>
2020-10-29 11:53:05 +0800

    fix modelbbox wrong label

--- samples/model-bbox/index.js
+++ samples/model-bbox/index.js
@@ -1,6 +1,6 @@
 var modelWidth = document.getElementById("Width");
 var modelLength = document.getElementById("Length");
-var modelHeight = document.getElementById("Hight");
+var modelHeight = document.getElementById("Height");
 var modelId = "Z83oJK8R";
 var appToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"; // A sample app token
