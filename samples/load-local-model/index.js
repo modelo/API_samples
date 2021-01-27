5c894408a69b940f8b9c56145198b537e161df8e
LynnYu17 <yujueaan@126.com>
2021-01-27 16:21:44 +0800

    new/load local model

---
+++ samples/load-local-model/index.js
@@ -0,0 +1,27 @@
+var modelId = "offlinemodel";
+//可自定义模型Id
+ appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"
+
+Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
+
+var viewer = new Modelo.View.Viewer3D("model", {
+  isMobile: isMobile()
+});
+
+viewer.loadModel(modelId, progress => {
+  // second parameter is an optional progress callback
+  
+  const c = document.getElementById("progress");
+  c.innerHTML = "Loading: " + Math.round(progress * 100) + "%";
+},true,true,'./sample').then(() => {
+  viewer.addInput(new Modelo.View.Input.Mouse(viewer));
+  viewer.addInput(new Modelo.View.Input.Touch(viewer));
+  // add keyboard callback.
+  const keyboard = new Modelo.View.Input.Keyboard(viewer);
+  viewer.addInput(keyboard);
+  keyboard.addKeyUpListener(keyboard => {
+      if (keyboard.key === 27) {
+          viewer.destroy();
+      }
+  });
+});
