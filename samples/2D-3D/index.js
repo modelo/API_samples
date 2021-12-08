57e6cb02bc66395e70f5a7302823e5fc226481a2
beiluo <beiluo@qunhemail.com>
2021-12-08 16:27:32 +0800

    优化2D-3D的案例

--- samples/2D-3D/index.js
+++ samples/2D-3D/index.js
@@ -1,5 +1,5 @@
-var modelId = "98yN2lrK";
-var appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUzLCJ1c2VybmFtZSI6Ik1vZGVsbyIsImlzUGVybWFuZW50Ijp0cnVlLCJpYXQiOjE1Njc1NjI0MTksImV4cCI6MzMxMDM1NjI0MTl9.EbW_cSPca4kWLedgNtfrGguog_o-3CCM5WhM7fFi0GA"
+var modelId = "x1qLqXrW";
+var appToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzMxLCJ1c2VybmFtZSI6ImxtIiwiaXNQZXJtYW5lbnQiOnRydWUsImlhdCI6MTYzNDYxMjQ2NSwiZXhwIjozMzE3MDYxMjQ2NX0.Q8IHwhg90aatmTeZChUthyhyt4KVAgKgf7LFMTgL0Ao"
 
 Modelo.init({ endpoint: "https://build-portal.modeloapp.com", appToken });
 
@@ -8,8 +8,8 @@ var oldTarget = null;
 const fill = '#00FF00';
 
 var viewer = new Modelo.View.Viewer3D("model", {
-  isMobile: isMobile(),
-  stencil: true
+    isMobile: isMobile(),
+    stencil: true
 });
 // model loaded successfully
 // add mouse to control camera.
@@ -21,106 +21,161 @@ selectElementTool.setEnabled(true);
 viewer.setEffectEnabled("Highlight", true);
 viewer.setEffectParameter("Highlight", "intensity", 1.0);
 viewer.getEventEmitter().on("onElementSelected", elementNames => {
-  const fill = '#00FF00';
+    const fill = '#00FF00';
 
-  if (oldElementName) {
-    viewer.getRenderScene().getEffect("Highlight").removeElements([oldElementName]);
-  }
-
-  if (oldTarget) {
-    for (var i = 0; i < oldTarget.children.length; i++) {
-      var child = oldTarget.children[i];
-      child.style.fill = "";
+    if (oldElementName) {
+        viewer.getRenderScene().getEffect("Highlight").removeElements([oldElementName]);
     }
-  }
 
-  if (elementNames.length === 0) {
-    return;
-  }
-
-  var strs = elementNames[0].split("/")
-  elementid = strs[strs.length -1];
-
-  viewer.getRenderScene().getEffect("Highlight").addElements([elementNames[0]], {
-    emissiveColor: [0.0, 1.0, 0.0]
-  });
-  oldElementName = elementNames[0];
-
-  var test = $("[elementid=" + elementid + "]" + "[linkid=0]");
-  if (test.length > 0) {
-    for (var i = 0; i < test[0].children.length; i++) {
-      var child = test[0].children[i];
-      child.style.fill = fill;
+    if (oldTarget) {
+        for (var i = 0; i < oldTarget.children.length; i++) {
+            var child = oldTarget.children[i];
+            child.style.fill = "";
+        }
     }
-    oldTarget = test[0];
-  }
+
+    if (elementNames.length === 0) {
+        return;
+    }
+
+    var strs = elementNames[0].split("/")
+    elementid = strs[strs.length - 1];
+
+    viewer.getRenderScene().getEffect("Highlight").addElements([elementNames[0]], {
+        emissiveColor: [0.0, 1.0, 0.0]
+    });
+    oldElementName = elementNames[0];
+
+    var test = $("[elementid=" + elementid + "]" + "[linkid=0]");
+    if (test.length > 0) {
+        for (var i = 0; i < test[0].children.length; i++) {
+            var child = test[0].children[i];
+            child.style.fill = fill;
+        }
+        oldTarget = test[0];
+    }
 });
 
-viewer
-  .loadModel(modelId, progress => {
+viewer.loadModel(modelId, progress => {
     // second parameter is an optional progress callback
     var c = document.getElementById("progress");
     c.innerHTML = "Loading: " + Math.round(progress * 100) + "%";
-  })
-  .then(() => {
+}).then(() => {
 
-  });
+});
 
-  Modelo.Model.get(modelId).then(function(res) {
-    var index = res.attachments.findIndex(function(item) { return item.filename === "floorPlans.json"});
-    if (index !== -1) {
-      // Get uploaded svg files.
-      $.get(res.attachments[index].url, function(data) {
-        var floorPlans = Object.values(data);
-        if (floorPlans.length === 0) {
-          return;
+Modelo.Model.get(modelId).then(function (res) {
+    $.ajaxSettings.async = false; 
+    var viewPlanFiles = [];
+    var len = res.attachments.length
+    
+    var compareAttachmentFileNameAndJsonItemName = (jsonItem, attachmentItem) => {
+        var strs = attachmentItem.filename.split("_");
+        return strs[strs.length - 1] === jsonItem.file;
+    };
+    var setViewList = (item, currentItemIndex, divName) => {
+        $.get(item.url, function (data) {
+            var floorPlans = Object.values(data);
+            if (floorPlans.length === 0) {
+                return;
+            }
+            for (const floorPlanItem of floorPlans) {
+                var index = viewPlanFiles.findIndex(function (viewPlanItem) {
+                    return compareAttachmentFileNameAndJsonItemName(floorPlanItem, viewPlanItem.item);
+                });
+
+                var setDiv = (index, itemName) => {                    
+                    $(divName).append(`<option value="${index}">${itemName}</option>`);
+                }
+
+                if (index !== -1) {
+                    var itemName = viewPlanFiles[index].item.filename.split("_")[0];
+                    setDiv(viewPlanFiles[index].i, itemName);
+                    viewPlanFiles.splice(index, 1);
+                } else {
+                    for (let j = currentItemIndex + 1; j < len; j++) {
+                        const attachmentItem = res.attachments[j];
+                        if (!compareAttachmentFileNameAndJsonItemName(floorPlanItem, attachmentItem)) { continue; }
+
+                        var itemName = attachmentItem.filename.split("_")[0];
+                        setDiv(j, itemName);
+                    }
+                }
+            }
+        }, "json");
+    };
+    for (let i = 0; i < len; i++) {
+        let item = res.attachments[i];
+        if (item.filename === "floorPlans.json") {
+            $("#viewType").append('<optgroup value="0" id="floorPlans" label="楼层平面视图"> </optgroup>');
+            setViewList(item, i, "#floorPlans");
+            continue;
         }
-        // Get the url of svg file.
-        index = res.attachments.findIndex(function(item) {
-          var strs = item.filename.split("_");
-          return strs[strs.length - 1] === floorPlans[0].file;
-        });
-        if (index !== -1) {
-          $.get(res.attachments[index].url, function(data){
+        if (item.filename === "elevations.json") {
+            $("#viewType").append('<optgroup value="1" id="elevations" label="立面视图"> </optgroup>');
+            setViewList(item, i, "#elevations");
+            continue;
+        }
+        if (item.filename === "sections.json") {
+            $("#viewType").append('<optgroup value="2" id="sections" label="剖面视图"></optgroup>');
+            setViewList(item, i, "#sections");
+            continue;
+        }
+        if (item.filename.substr(item.filename.lastIndexOf('.'))===".svg"){            
+            viewPlanFiles.push({ item, i });
+        }
+    }
+
+    return res
+}).then((res) => {
+    $.ajaxSettings.async = true; 
+    var setView = (url) => {
+        $.get(url, function (data) {
             document.getElementById("svg-container").innerHTML = data;
 
             window.addEventListener('mouseup', (e) => {
-              var target = e.target;
-              while(target && target.tagName !== "g") {
-                target = target.parentElement;
-              } 
-              if (!target) {
-                return;
-              }
-              var elementName = modelId + "+" + target.attributes.linkid.nodeValue + "/" + target.attributes.elementid.nodeValue;
-
-              if (oldElementName) {
-                viewer.getRenderScene().getEffect("Highlight").removeElements([oldElementName]);
-              }
-              if (oldTarget) {
-                for (var i = 0; i < oldTarget.children.length; i++) {
-                  var child = oldTarget.children[i];
-                  child.style.fill = "";
+                var target = e.target;
+                while (target && target.tagName !== "g") {
+                    target = target.parentElement;
                 }
-              }
+                if (!target) {
+                    return;
+                }
+                var elementName = modelId + "+" + target.attributes.linkid.nodeValue + "/" + target.attributes.elementid.nodeValue;
 
-              oldElementName = elementName;
-              
-              selectElementTool.pick([elementName], true);
+                if (oldElementName) {
+                    viewer.getRenderScene().getEffect("Highlight").removeElements([oldElementName]);
+                }
+                if (oldTarget) {
+                    for (var i = 0; i < oldTarget.children.length; i++) {
+                        var child = oldTarget.children[i];
+                        child.style.fill = "";
+                    }
+                }
 
-              viewer.getRenderScene().getEffect("Highlight").addElements([elementName], {
-                emissiveColor: [0.0, 1.0, 0.0]
-              });
+                oldElementName = elementName;
 
-              for (var i = 0; i < target.children.length; i++) {
-                var child = target.children[i];
-                child.style.fill = fill;
-              }
-              oldTarget = target;
+                selectElementTool.pick([elementName], true);
+
+                viewer.getRenderScene().getEffect("Highlight").addElements([elementName], {
+                    emissiveColor: [0.0, 1.0, 0.0]
+                });
+
+                for (var i = 0; i < target.children.length; i++) {
+                    var child = target.children[i];
+                    child.style.fill = fill;
+                }
+                oldTarget = target;
             })
 
-          }, "text");
-        }
-      }, "json");
+        }, "text");
     }
-  });
+    
+    let selectedIndex = parseInt($("select#viewType option:selected").val());
+    setView(res.attachments[selectedIndex].url);
+    
+    $("select#viewType").change(function () {
+        var index = parseInt($(this).val());
+        setView(res.attachments[index].url);
+    });
+});
